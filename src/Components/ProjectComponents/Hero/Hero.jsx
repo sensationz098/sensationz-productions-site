4713a895dc639174c94f9e1c51032e76c2c21397
NipunShrivats <nipunrawat16@gmail.com>
2024-10-01 16:46:18 +0530

    changes on 1oct, commit no.10

--- src/Components/ProjectComponents/Hero/Hero.jsx
+++ src/Components/ProjectComponents/Hero/Hero.jsx
@@ -1,12 +1,12 @@
 import React from "react";
 import "./Hero.css";
+import banner from "../../../assets/projects-img/projects-banner.png";
 export default function Hero(props) {
   const { color, title, image } = props;
   return (
     <>
       <div className="hero-main" style={{ backgroundColor: "" }}>
-        {/* <img src="" alt="" /> */}
-        <h1 className="title-hero">{title}</h1>
+        <img src={banner} alt="" />
       </div>
     </>
   );
